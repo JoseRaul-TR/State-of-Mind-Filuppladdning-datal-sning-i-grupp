f3a6c1661f16138786db1d379d3148391faa8303
Linnéa Toth <linnea@liroit.com>
2025-09-25 15:40:40 +0200

    bugfix empty cells

--- src/utils/workbookToRows.js
+++ src/utils/workbookToRows.js
@@ -15,14 +15,13 @@ export function workbookToRows(workbook) {
         const obj = {}; //Objekt för varje rad, som kommer att innehålla key för kolumnen och value för varje cells faktiska värde, blir typ såhär: {Column_1: "Cellvärde", column_2: "Nästa cellvärde"  .. osv}
 
         for (let col = 1; col <= maxCols; col++) {
-            const cell = row.getCell(col).text; //getCell(<kolumn>) är också en inbyggd ExcelJS-metod, om det ligger något annat än en sträng i cellen så kommer .text att returnera den rimligaste tolkningen i text, om jag har förstått det rätt
-            let cellVal = cell?.value ?? ""; //nullish coalescing operator (= ??) returnerar det vänstra värdet om inte det är null eller undefined, annars skickar den tillbaka det högra värdet (i det här fallet en tom sträng). 
+            const cellVal = row.getCell(col).text ?? ""; //getCell(<kolumn>) är också en inbyggd ExcelJS-metod, om det ligger något annat än en sträng i cellen så kommer .text att returnera den rimligaste tolkningen i text, om jag har förstått det rätt. Nullish coalescing operator (= ??) returnerar det vänstra värdet om inte det är null eller undefined, annars skickar den tillbaka det högra värdet (i det här fallet en tom sträng). 
 
-            obj[`Column_${col}`] = String(cellVal);
+            obj[`Column_${col}`] = cellVal;
         }
 
         rows.push(obj);
-        console.log(obj);
+
     });
 
     return rows;
