f29127a5480067da536281c351f89e3b3a9e2e7c
José_Raúl Tenza_Ramírez <jraultr@gmail.com>
2025-09-26 14:39:44 +0200

    ExportButton styles

--- src/components/ExportButton.jsx
+++ src/components/ExportButton.jsx
@@ -3,11 +3,11 @@ import { AiOutlineFilePdf } from "react-icons/ai";
 
 export default function ExportButton({ onExport }) {
   return (
-    <div className="fixed right-6 bottom-25 z-40">
+    <div className="fixed right-20 bottom-25 z-40">
       <Button
         clickHandler={onExport}
-        className="flex !h-18 !w-18 items-center justify-center !rounded-full !p-4 !shadow-xl"
-        buttonText={<AiOutlineFilePdf className="text-4xl text-white" />}
+        className="flex !h-25 !w-25 items-center justify-center !rounded-full !p-5 !shadow-xl"
+        buttonText={<AiOutlineFilePdf className="text-6xl text-white" />}
       />
     </div>
   );
